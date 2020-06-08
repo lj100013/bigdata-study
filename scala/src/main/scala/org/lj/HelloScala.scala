ebe411530253190f2c440858d7d2dbf71f5a6ed9
lj100013 <729251231@qq.com>
2020-06-08 23:28:08 +0800

    object底层编译后底层java原理

--- scala/src/main/scala/org/lj/HelloScala.scala
+++ scala/src/main/scala/org/lj/HelloScala.scala
@@ -1,5 +1,11 @@
 package org.lj
 
+/*
+/ object 编译后底层生成两个对象 HelloScala.class   HelloScala$.class
+  HelloScala.class 调用 HelloScala$.class自身的 静态 单例 对象 module$
+    public static final  MODULE$;
+    private HelloScala$() { MODULE$ = this; --构造方法私有,无法new 进行实例化
+*/
 object HelloScala {
   def main(args: Array[String]): Unit = {
     println("helle scala")
